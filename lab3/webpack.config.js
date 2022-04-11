4c2df268537fab8bdfa2adb7d7aeb5d1b4161a99
XioRi007 <mariyaivanova@ukr.net>
2022-04-11 10:10:23 +0300

    lab3

---
+++ lab3/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//var CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const WriteFilePlugin  = require('write-file-webpack-plugin');
+
+let pages = ["index","news", "photo", "rozklad"].map(element =>{
+  return new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, `./src/pages/${element}.html`), 
+    filename: `${element}.html`
+    });
+});
+module.exports = {
+  entry: {
+    index:'./src/index.js'
+  },
+
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js',
+   // assetModuleFilename: './assets/images/[hash][ext][query]'
+  },
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+
+  plugins: [
+    ...pages,
+  WriteFilePlugin(),
+  new CopyPlugin({
+    patterns: [     
+      { from: 'src/pages/assets/images', to: 'assets/images' },
+   
+      
+    ],
+  }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+  }),    
+
+    ]
+  
+};
