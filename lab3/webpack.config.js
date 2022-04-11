943a0fabc755a93d8eccc9dd049e2b229d266afd
XioRi007 <mariyaivanova@ukr.net>
2022-04-11 10:34:43 +0300

    lab3

--- lab3/webpack.config.js
+++ lab3/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-//var CopyWebpackPlugin = require('copy-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
 const WriteFilePlugin  = require('write-file-webpack-plugin');
