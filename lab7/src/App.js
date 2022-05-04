35816f6a6943326a663df7d35b40645e7e275e64
XioRi007 <mariyaivanova@ukr.net>
2022-05-04 11:21:41 +0300

    adding lab7

--- lab7/src/App.js
+++ lab7/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import SliderPage from './pages/SliderPage';
